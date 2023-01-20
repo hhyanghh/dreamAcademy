cdcbb202d4f8ccf5f49a17b8f50f20f75e5a34f0
양현화 <yanghh711@naver.com>
2023-01-20 12:42:33 +0900

    부모, 자식 각각의 상태관리 props

--- basic/src/AppCounter.jsx
+++ basic/src/AppCounter.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { React, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 export default function AppCounter() {
+  const [total, setTotal] = useState(0);
+  const handleClick = () => {
+    setTotal((prev) => prev + 1);
+  };
   return (
-    <div>
-      <Counter />
+    <div className="container">
+      <div className="banner">
+        Total Count: {total} {total > 10 ? "🔥" : "❄️"}
+      </div>
+      <div>
+        <Counter total={total} handleClick={handleClick} />
+        <Counter total={total} handleClick={handleClick} />
+      </div>
     </div>
   );
 }
--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,20 +1,17 @@
 import { React, useState } from "react";
 
-export default function Counter() {
-  const [num, setNum] = useState(0);
+export default function Counter({ total, handleClick }) {
+  const [count, setCount] = useState(0);
   return (
     <div className="counter">
-      <span className="number">{num}</span>
+      <p className="number">
+        {count} <span className="total"> /{total}</span>
+      </p>
       <button
         className="button"
         onClick={() => {
-          setNum(num + 5);
-          // setNum((prev) => prev + 1);
-          // setNum((prev) => prev + 1);
-          // setNum((prev) => prev + 1);
-          // setNum((prev) => prev + 1);
-          // setNum((prev) => prev + 1);
-          console.log(num);
+          setCount((prev) => prev + 1);
+          handleClick();
         }}
       >
         Add +
