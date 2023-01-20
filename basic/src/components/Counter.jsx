7696ce5d6399afc7cc2867f97ef89c2915ebbcb1
양현화 <yanghh711@naver.com>
2023-01-20 11:03:38 +0900

    useState count 증가

--- basic/src/components/Counter.jsx
+++ basic/src/components/Counter.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
 
 export default function Counter() {
+  const [num, setNum] = useState(0);
   return (
     <div className="counter">
-      <span className="number">0</span>
-      <button className="button">Add +</button>
+      <span className="number">{num}</span>
+      <button
+        className="button"
+        onClick={() => {
+          setNum(num + 5);
+          // setNum((prev) => prev + 1);
+          // setNum((prev) => prev + 1);
+          // setNum((prev) => prev + 1);
+          // setNum((prev) => prev + 1);
+          // setNum((prev) => prev + 1);
+          console.log(num);
+        }}
+      >
+        Add +
+      </button>
     </div>
   );
 }
