abe2832dba09fbbdbbc1cf447fd315f051587b5d
양현화 <yanghh711@naver.com>
2023-01-21 09:18:46 +0900

    객체로 상태 관리하기

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -2,20 +2,20 @@ import { React, useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   return (
     <div
       className="container"
       onPointerMove={(e) => {
         console.log(e.clientX, e.clientY);
-        setX(e.clientX);
-        setY(e.clientY);
+        setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        // setPosition((prev) => ({ x: e.clientX, y: prev.y }));
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       />
     </div>
   );
