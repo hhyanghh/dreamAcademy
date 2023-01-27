ff72f89156055c935c33bc22dfc1d639c1054708
hhyanghh <yanghh711@naver.com>
2023-01-27 09:55:10 +0900

    멘토 추가/삭제하기 기능 추가

--- basic/src/AppMentors.jsx
+++ basic/src/AppMentors.jsx
@@ -49,6 +49,30 @@ export default function AppMentors() {
       >
         멘토의 이름을 바꾸기
       </button>
+      <button
+        onClick={() => {
+          const name = prompt("추가하는 이름");
+          const title = prompt("추가하는 타이틀");
+
+          setPerson((person) => ({
+            ...person,
+            mentors: [...person.mentors, { name, title }],
+          }));
+        }}
+      >
+        멘토 추가하기
+      </button>
+      <button
+        onClick={() => {
+          const name = prompt("누구를 삭제하고 싶은가요?");
+          setPerson((person) => ({
+            ...person,
+            mentors: person.mentors.filter((m) => m.name !== name),
+          }));
+        }}
+      >
+        멘토 삭제하기
+      </button>
     </>
   );
 }
