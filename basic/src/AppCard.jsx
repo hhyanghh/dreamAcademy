35bc8e2ef8f26318b6b67a8a8b85090ddc10c48a
hhyanghh <yanghh711@naver.com>
2023-01-28 09:27:23 +0900

    컴포넌트 재사용 카드

---
+++ basic/src/AppCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function AppCard() {
+  return (
+    <>
+      <Card>
+        <p>card1</p>
+      </Card>
+      <Card>
+        <h2>Card2</h2>
+        <p>설명2</p>
+      </Card>
+    </>
+  );
+}
+
+function Card({ children }) {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        borderRadius: "20px",
+        color: "white",
+        minHeight: "200px",
+        maxWidth: "200px",
+        margin: "1rem",
+        padding: "1rem",
+        textAlign: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
--- basic/src/index.js
+++ basic/src/index.js
@@ -11,11 +11,12 @@ import AppMentors from "./AppMentors";
 import AppMentorsImmer from "./AppMentorsImmer";
 import AppForm from "./AppForm";
 import AppWrap from "./AppWrap";
+import AppCard from "./AppCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppWrap />
+    <AppCard />
   </React.StrictMode>
 );
 
