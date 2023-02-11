aa82fa7112d1e3a6b966c064b823d5019d27cfd6
hhyanghh <yanghh711@naver.com>
2023-02-11 22:27:21 +0900

    입력폼 스타일링

--- todo-app/src/components/AddTodo/AddTodo.jsx
+++ todo-app/src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,7 @@
+import { style } from "@mui/system";
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodo.module.css";
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState("");
@@ -13,14 +15,17 @@ export default function AddTodo({ onAdd }) {
     setText("");
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleChange}
       />
-      <button type="submit">ADD</button>
+      <button type="submit" className={styles.button}>
+        ADD
+      </button>
     </form>
   );
 }
