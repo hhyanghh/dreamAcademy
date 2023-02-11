af17a07c011413ca43e13bf74322191eb27ef174
hhyanghh <yanghh711@naver.com>
2023-02-11 22:42:47 +0900

    투두 아이템 스타일링

--- todo-app/src/components/Todo/Todo.jsx
+++ todo-app/src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaRegTrashAlt } from "react-icons/fa";
+import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { id, text, status } = todo;
@@ -9,17 +10,22 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   };
   const handleDelete = () => onDelete(todo);
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
         id={id}
         checked={status === "completed"}
         onChange={handleChange}
       />
-      <label htmlFor={id}>{text}</label>
-      <button onClick={handleDelete}>
-        <FaRegTrashAlt />
-      </button>
+      <label htmlFor={id} className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <FaRegTrashAlt />
+        </button>
+      </span>
     </li>
   );
 }
