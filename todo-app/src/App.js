ccfbca55be524ecfc18dcc8a0b73e3dd85564256
hhyanghh <yanghh711@naver.com>
2023-02-12 15:49:28 +0900

    다크모드 구현하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -10,19 +10,20 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 import TodoList from "./components/TodoList/TodoList";
 import Header from "./components/Header/Header";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
         onFilterChange={(filter) => setFilter(filter)}
       />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
