682ae658baac4819bbc260f26f0a4cbd09d8979a
hhyanghh <yanghh711@naver.com>
2023-02-12 20:59:21 +0900

    router outlet

--- router/src/App.js
+++ router/src/App.js
@@ -1,8 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/videos",
+        element: <Videos />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <p>App</p>;
+  return <RouterProvider router={router}>App</RouterProvider>;
 }
 
 export default App;
