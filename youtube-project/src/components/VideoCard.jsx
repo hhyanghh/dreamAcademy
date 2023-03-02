5823a9c79a2e1cbac6c6120a50aa4ae5567161ed
hhyanghh <yanghh711@naver.com>
2023-03-02 23:00:22 +0900

    상세페이지 스타일링

--- youtube-project/src/components/VideoCard.jsx
+++ youtube-project/src/components/VideoCard.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { formatAgo } from "../util/date";
 
-export default function VideoCard({ video }) {
+export default function VideoCard({ video, type }) {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate(`/videos/watch/${video.id}`, { state: { video } });
   };
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const isList = type === "list";
   return (
-    <li onClick={handleClick}>
-      <img className="w-full" src={thumbnails.medium.url} alt={title} />
+    <li onClick={handleClick} className={isList ? "flex gap-1 m-2" : ""}>
+      <img
+        className={isList ? "w-60 mr-2" : "w-full"}
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
         <p className="font-sm opacity-80">{channelTitle}</p>
