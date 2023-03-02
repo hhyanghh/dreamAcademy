9e99348e4686d128d74aaf121d84b5ffc995b70e
hhyanghh <yanghh711@naver.com>
2023-03-02 20:57:07 +0900

    비디오 목록 가져오기 리펙토링

--- youtube-project/src/pages/Videos.jsx
+++ youtube-project/src/pages/Videos.jsx
@@ -2,20 +2,16 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
-import { search } from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
-import Youtube from "../api/youtube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new Youtube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
   return (
     <>
       <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>
