5f03a4ce19b90cf0f34ce9d2ec9a9cf22d8cdc16
hhyanghh <yanghh711@naver.com>
2023-03-02 22:49:42 +0900

    채널정보, 연관 비디오 가져오기

--- youtube-project/src/components/RelatedVideos.jsx
+++ youtube-project/src/components/RelatedVideos.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "./VideoCard";
 
 export default function RelatedVideos({ id }) {
-  return <div>{id}</div>;
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["related", id], () => youtube.relatedVideos(id));
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>에러발생</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
