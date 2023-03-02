f2b66449586ac03b5907cc6fb47ba689a7e9b3b7
hhyanghh <yanghh711@naver.com>
2023-03-02 22:11:47 +0900

    디테일 페이지 골격

--- youtube-project/src/components/VideoCard.jsx
+++ youtube-project/src/components/VideoCard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { formatAgo } from "../util/date";
 
 export default function VideoCard({ video }) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/videos/watch/${video.id}`, { state: { video } });
+  };
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
   return (
-    <li>
+    <li onClick={handleClick}>
       <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
