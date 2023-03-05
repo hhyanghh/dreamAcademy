f26d0aa254e1fdce0afac06ba9b4599b9c8c9273
hhyanghh <yanghh711@naver.com>
2023-03-05 13:58:05 +0900

    구글 로그인 구현

---
+++ shoppingmall/src/api/firebase.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  getAuth,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATEBASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export async function login() {
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+      return user;
+    })
+    .catch(console.error);
+}
+
+export async function logout() {
+  return signOut(auth)
+    .then(() => null)
+    .catch(console.error);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+}
--- shoppingmall/src/components/Navbar.jsx
+++ shoppingmall/src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
 export default function Header() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -16,8 +32,9 @@ export default function Header() {
         <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
-        <div>계정</div>
-        <button>Login</button>
+        {user && <div>{user.displayName}</div>}
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
