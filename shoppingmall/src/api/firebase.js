ca4d14c753d3ff9d1ba791feaae64779cc99d1ef
hhyanghh <yanghh711@naver.com>
2023-03-05 15:18:19 +0900

    admin 계정 확인하기

--- shoppingmall/src/api/firebase.js
+++ shoppingmall/src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import { getDatabase, ref, onValue, get } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,6 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
 provider.setCustomParameters({
   prompt: "select_account",
@@ -31,7 +33,24 @@ export function logout() {
 }
 
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    // 1. 사용자가 있는 경우 (로그인 한 경우)
+    const updatedUser = user ? await adminUser(user) : null;
+
+    callback(updatedUser);
   });
+}
+
+async function adminUser(user) {
+  // 2. 사용자가 어드민 권한을 가지고 있는지 확인
+  // 3. {...user, isAdmin: true/false}
+  return get(ref(database, "admins")) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+      }
+      return user;
+    });
 }
--- shoppingmall/src/components/Navbar.jsx
+++ shoppingmall/src/components/Navbar.jsx
@@ -9,7 +9,10 @@ export default function Header() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    onUserStateChange(setUser);
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
   }, []);
 
   return (
