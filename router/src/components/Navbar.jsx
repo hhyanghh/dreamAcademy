6ab20b460939ae6a1e4731f8a034e7b65c816921
hhyanghh <yanghh711@naver.com>
2023-06-25 09:37:21 +0900

    react-router-dom 활용

--- router/src/components/Navbar.jsx
+++ router/src/components/Navbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useSearchParams,
+  useParams,
+} from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = useParams();
+
+  console.log(location, "location");
+  console.log(searchParams.get("test"), "searchParams");
+  console.log(params, "params");
+
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="videos">Videos</Link>
+      <Link to="?test=1111">Query String</Link>
     </nav>
   );
 }
